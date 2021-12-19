45acc0387ec4974bd78a7e50632e6b0ec2cd11e8
Adrian Rybka <adrian_rybka1989@wp.pl>
2021-12-19 15:51:33 +0100

    Commit 19-12-2021

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,14 +1,19 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"../model/formatter"
-], function(Controller, formatter) {
+	"../model/formatter",
+	"sap/ui/model/json/JSONModel"
+], function(Controller, formatter, JSONModel) {
 	"use strict";
 
-	return Controller.extend("sap.ui.demo.basicTemplate.controller.App", {
+	return Controller.extend("sap.ui.demo.basicTemplate.controller.Home", {
 
 		formatter: formatter,
 
 		onInit: function () {
+
+			var sPath = jQuery.sap.getModulePath("sap.ui.demo.basicTemplate", "/model/Tree.json");
+			var oModel = new JSONModel(sPath);
+			this.getView().setModel(oModel, "Tree");
 
 		}
 	});
